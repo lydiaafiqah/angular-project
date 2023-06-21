dc075fbb74927a2e31cd6bbe97757bbbbea83f8a
lydiaafiqah <60863233+lydiaafiqah@users.noreply.github.com>
2023-06-21 15:54:34 +0800

    Setting up project

--- hello-world/src/app/app.component.spec.ts
+++ hello-world/src/app/app.component.spec.ts
@@ -1,10 +1,8 @@
 import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(() => TestBed.configureTestingModule({
-    imports: [RouterTestingModule],
     declarations: [AppComponent]
   }));
 
@@ -14,16 +12,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-beginner'`, () => {
+  it(`should have as title 'hello-world'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-beginner');
+    expect(app.title).toEqual('hello-world');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-beginner app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('hello-world app is running!');
   });
 });
